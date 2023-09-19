2ccff1bb7389e3ea1cd0b0cd34cc376f79afcb6c
ArpitGoyal8811 <500094790@stu.upes.ac.in>
2023-09-19 09:55:41 +0530

    initial commit

---
+++ src/main/java/cicd/lab3/myclass.java
@@ -0,0 +1,14 @@
+package cicd.lab3;
+
+public class myclass {
+
+	public void greet(){
+		System.out.println("Hello Github Actions");
+	}
+	public static void main(String[] args) {
+		myclass obj= new myclass();
+		obj.greet();
+
+	}
+
+}
